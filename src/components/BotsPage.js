8c52f14d331c13205e856b7ccd60cbbf0ad33cd3
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-10-03 06:28:25 +0300

    Added useState to update botData in state

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,9 +1,12 @@
+
+
 import React from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
   //start here with your code for step one
+  const [botData, setBotData] = useState([]);
 
   return (
     <div>
